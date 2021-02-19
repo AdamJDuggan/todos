ce8d144132ccce09795a1a84b172d5a3bcfb5499
AdamJDuggan <adamduggan17@gmail.com>
2021-02-19 13:09:40 +0000

    add deps

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Adam
+          Learn React Adam!
         </a>
       </header>
     </div>
